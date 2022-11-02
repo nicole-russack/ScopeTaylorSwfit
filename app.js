94ab5e5564706a6d05050c9ee9bd3da9873729b8
Nicole Russack <russack@usc.edu>
2022-11-01 20:11:26 -0700

    output page has an issue

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const path = require("path")
-
 const publicDirectoryPath = path.join(__dirname, './public')
 const app = express()
+app.use(express.static(publicDirectoryPath))
+app.set('view engine', 'hbs')
 
 app.use(express.static(publicDirectoryPath))
 
@@ -12,10 +13,13 @@ app.get('', (req, res) => {
 })
 
 app.get('about', (req, res) => {
-    
+   
 })
 
-app.get('output', (req, res) => {
+app.get('/output', (req, res) => {
+    res.render('output', {
+        quote:"this wil change"
+    })
    
 })
 
